eafa12b661891f01384d50cb534b54ec71d60ba5
IT21209420 <99248793+IT21209420@users.noreply.github.com>
2023-05-15 11:29:36 +0530

    commit plans

--- app/src/main/java/com/example/espark/fragments/adapters/Plan.kt
+++ app/src/main/java/com/example/espark/fragments/adapters/Plan.kt
@@ -1,6 +1,6 @@
 package com.example.espark.fragments.adapters
 
-data class Plan(//Plan data class
+data class Plan(//Plans data class
     var planName: String? = null,
     val amount: String? = null,
     val additems: MutableList<AddItem>,
