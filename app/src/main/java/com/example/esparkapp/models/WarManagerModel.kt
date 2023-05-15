73f1f777210d276a3af46d43ef7ac273578103ab
Chamikara Vithanage <it21219498@my.sliit.lk>
2023-05-15 12:14:33 +0530

    model class added

---
+++ app/src/main/java/com/example/esparkapp/models/WarManagerModel.kt
@@ -0,0 +1,26 @@
+package com.example.esparkapp.models
+
+import java.util.concurrent.TimeUnit
+
+data class WarManagerModel (
+    var wmId:String? = null,
+    var ProName:String? = null,
+    var BraName:String? = null,
+    var PurchDate:String? = null,
+    var WarPeriod:String? = null,
+    var Price:String? = null,
+    var PurchLocation:String? = null,
+    var PhoneNo:String? = null,
+    var Email:String? = null,
+    var Other:String? = null,
+    val itemImg:String?= null,
+
+
+    )
+//{
+//    val expiryDate: Long
+//        get() = PurchDate + TimeUnit.DAYS.toMillis(WarPeriod.toLong())
+//
+//    val isActive: Boolean
+//        get() = expiryDate > System.currentTimeMillis()
+//}
